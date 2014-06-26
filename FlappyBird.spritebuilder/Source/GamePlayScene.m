28ecf20adec765288dfdd2331f853865f1793b54
ayan <stormfrog.ayan@gmail.com>
2014-06-27 00:12:05 +0600

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -18,7 +18,6 @@
 -(void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
 {
     // this will get called every time the player touches the screen
-//    NSLog(@"test");
     [character flap];
 }
 
